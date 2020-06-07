d9dddf4a12fe045f551c3969e4bb8801e2896ad5
linncy <linncy@itcast.cn>
2020-06-07 18:02:18 +0800

    初始化之后

---
+++ src/components/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>默认模板</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  create() {},
+  methods: {}
+}
+</script>
+<style lang='less' scoped>
+</style>
